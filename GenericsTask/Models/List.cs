36772228e5406bb7108acb985f20d98b18129df8
MelekAzizova <147470006+MelekAzizova@users.noreply.github.com>
2023-11-05 23:31:55 +0400

    Add project files.

---
+++ GenericsTask/Models/List.cs
@@ -0,0 +1,91 @@
+﻿namespace GenericsTask.Models;
+
+internal class List<T>
+{
+    T[] _arr=new T[0];
+    public int Capacity { get; set; } 
+    public int Count { get; set; }=0;
+    public List()
+    {
+        
+    }
+    public List(int capacity)
+    {
+        Capacity = capacity <= 0? 10: capacity;
+    }
+    public List(int capacity,params T[] array)
+    {
+        _arr = array;
+        
+        Count = array.Length;
+        
+    }
+
+
+    public void Add(T array)
+    {
+        if (Count == _arr.Length)
+        {
+            Array.Resize(ref _arr, Count + Capacity);
+
+        }
+        _arr[Count++] = array;
+       
+    }
+
+    public void Remove(T[] array, int index)
+    {
+        for (int i = index; i < _arr.Length - 1; i++)
+        {
+            _arr[i] = _arr[i + 1];
+        }
+        Array.Resize(ref _arr, Count - 1);
+
+    }
+
+    public void Reverse()
+    {
+
+    }
+    public void IndexOf(T[] arr,int searchNums)
+    {
+        int index = Array.IndexOf(arr, searchNums);
+        if (index > 0 )
+        {
+            Console.WriteLine("number: "+searchNums+"index"+arr);
+        }
+        else
+        {
+            Console.WriteLine("Bele bir element tapilmadi");
+        }
+        
+        
+
+    }
+
+    public void LastIndexOf(T[] array,int nums)
+    {
+        int index=Array.LastIndexOf(array,nums);
+        if(index > 0)
+        {
+            Console.WriteLine(array);
+        }
+        else
+        {
+
+            Console.WriteLine("Bele bir element tapilmadi");
+        }
+       
+    }
+
+    public void Clear(T[] array)
+    {
+        
+
+    }
+    //public bool Exits(int arr)
+    //{
+        
+
+    //}
+}
---
+++ GenericsTask/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace GenericsTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>(){ 2,5,6,9,12} ;
+            
+          
+            Console.WriteLine("List: ");
+            foreach (int i in list)
+            {
+               
+                Console.WriteLine(i);
+     
+            }
+
+
+
+            list.Add(20);
+            list.Add(51);
+            list.Add(19);
+            list.Add(2);
+
+
+            Console.WriteLine("ListAdd: ");
+            foreach (int i in list)
+            {
+
+                Console.WriteLine(i);
+
+            }
+
+            list.Remove(20);
+            list.Remove(5);
+            Console.WriteLine("ListRemove: ");
+            foreach (int i in list)
+            {
+
+                Console.WriteLine(i);
+
+            }
+            Console.WriteLine("Enter to number: ");
+            int number=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(list.IndexOf(number));
+
+            Console.WriteLine("Enter to number: ");
+            int num= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(list.LastIndexOf(2));
+
+
+
+
+
+
+
+
+        }
+    }
+}
